ca5ea1bd1f6086b6a55329a52b771b8ee923f9c9
Ильшат Кабиров <ilshatanet@gmail.com>
2022-09-01 16:41:01 +0800

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,25 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import {MainPage, ComicsPage, Page404, SingleComicPage} from '../pages';
+import AppHeader from "../appHeader/AppHeader";
+
+const App = () => {
+    
+    return (
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<MainPage/>} />
+                        <Route path="/comics" element={<ComicsPage/>} />
+                        <Route path="/comics/:comicId" element={<SingleComicPage/>} />
+                        <Route path="*" element={<Page404/>} />
+                    </Routes>
+                </main>
+            </div>
+        </Router>
+    )
+}
+
+export default App;
---
+++ src/components/appHeader/AppHeader.js
@@ -0,0 +1,23 @@
+import {Link, NavLink} from 'react-router-dom';
+import './appHeader.scss';
+
+const AppHeader = () => {
+    return (
+        <header className="app__header">
+            <h1 className="app__title">
+                <Link to="/">
+                    <span>Marvel</span> information portal
+                </Link>
+            </h1>
+            <nav className="app__menu">
+                <ul>
+                    <li><NavLink style={({isActive}) => isActive ? {'color': '#9f0013'} : null} to="/">Characters</NavLink></li>
+                    /
+                    <li><NavLink style={({isActive}) => isActive ? {'color': '#9f0013'} : null} to="/comics">Comics</NavLink></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default AppHeader;
